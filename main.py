9cabfb15806be9fcd279ac7ccf4fcd071041348e
yangkuan <yangkuan@DESKTOP-O8R8OPN>
2017-09-13 10:03:18 +0800

    urllib

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from utils import mssql, pyredis, mongodb
+from urllib import request
+
+response=request.urlopen("http://www.baidu.com")
+print(response.read().decode("utf-8"))
 
 '''
-mongo演示
-'''
+#mongo演示
 mongodb.HOST = "192.168.158.130"
 mongodb.PORT = 27017
 mongodb.DB = "test"
@@ -12,18 +15,14 @@ for item in mongodb.find():
     for (key, value) in item.items():
         print(key, value)
 
-'''
-redis演示
-'''
+#redis演示
 pyredis.HOST = "192.168.158.130"
 pyredis.PORT = 6379
 
 pyredis.setvalue("key", "ewfwef323")
 pyredis.getvalue("key")
 
-'''
-SQL SERVER演示
-'''
+#SQL SERVER演示
 mssql.HOST = "127.0.0.1:62331"
 mssql.USER = "sa"
 mssql.PASSWORD = "1"
@@ -32,3 +31,5 @@ mssql.DATABASE = "xindata"
 for row in mssql.exsql("select * from users"):
     for col in row:
         print(col)
+
+'''
